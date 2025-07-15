1960eaf5f750b2f2066e5f77fd96d598a5da94bf
XiaoHuNao <65500205+XiaoHuNao@users.noreply.github.com>
2025-07-15 23:32:37 +0800

    移动哥布林军团Bar

--- src/main/java/com/xiaohunao/heaven_destiny_moment/common/init/HDMBarRenderTypes.java
+++ src/main/java/com/xiaohunao/heaven_destiny_moment/common/init/HDMBarRenderTypes.java
@@ -3,7 +3,6 @@ package com.xiaohunao.heaven_destiny_moment.common.init;
 import com.xiaohunao.heaven_destiny_moment.HeavenDestinyMoment;
 import com.xiaohunao.heaven_destiny_moment.client.gui.bar.render.DefaultBarRenderType;
 import com.xiaohunao.heaven_destiny_moment.client.gui.bar.render.IBarRenderType;
-import com.xiaohunao.heaven_destiny_moment.client.gui.bar.render.SlimeRainBarRenderType;
 import com.xiaohunao.xhn_lib.api.register.FlexibleHolder;
 import com.xiaohunao.xhn_lib.api.register.FlexibleRegister;
 
@@ -11,6 +10,5 @@ public class HDMBarRenderTypes {
     public static final FlexibleRegister<IBarRenderType> BAR_RENDER_TYPE = FlexibleRegister.create(HDMRegistries.BAR_RENDER_TYPE, HeavenDestinyMoment.MODID);
 
     public static final FlexibleHolder<IBarRenderType, ?> DEFAULT_BAR_RENDER_TYPE = BAR_RENDER_TYPE.registerStatic("default", DefaultBarRenderType::new);
-    public static final FlexibleHolder<IBarRenderType, ?> SLIME_BAR_RENDER_TYPE = BAR_RENDER_TYPE.registerStatic("slime_rain", SlimeRainBarRenderType::new);
 
 }
