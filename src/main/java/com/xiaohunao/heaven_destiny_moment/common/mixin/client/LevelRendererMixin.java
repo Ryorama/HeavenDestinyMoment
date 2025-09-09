09c9ac03dad8d795a9c791160844468e23057462
westernat <westernat233@163.com>
2025-09-09 10:27:14 +0800

    使用LocalRef

--- src/main/java/com/xiaohunao/heaven_destiny_moment/common/mixin/client/LevelRendererMixin.java
+++ src/main/java/com/xiaohunao/heaven_destiny_moment/common/mixin/client/LevelRendererMixin.java
@@ -1,7 +1,10 @@
 package com.xiaohunao.heaven_destiny_moment.common.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.xiaohunao.heaven_destiny_moment.common.context.ClientMoonSettings;
 import com.xiaohunao.heaven_destiny_moment.common.context.ClientSettings;
@@ -11,6 +14,8 @@ import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,19 +25,23 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.Nullable;
-
 @Mixin(LevelRenderer.class)
 public abstract class LevelRendererMixin {
     @Shadow
     @Nullable
     private ClientLevel level;
 
-    @ModifyConstant(method = "renderSky", constant = @Constant(floatValue = 20.0F))
-    private float renderSky(float originalSize) {
-        MomentInstance instance = MomentInstanceManager.of(level).getClientMomentInstance();
+    @ModifyExpressionValue(method = "renderSky", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getSkyColor(Lnet/minecraft/world/phys/Vec3;F)Lnet/minecraft/world/phys/Vec3;"))
+    private Vec3 cacheInstance(Vec3 original, @Share("momentInstance") LocalRef<@Nullable MomentInstance> ref) {
+        ref.set(MomentInstanceManager.of(level).getClientMomentInstance());
+        return original;
+    }
 
-        if (instance != null) originalSize = instance.getMoment().clientSettings()
+    @ModifyConstant(method = "renderSky", constant = @Constant(floatValue = 20.0F))
+    private float renderSky(float originalSize, @Share("momentInstance") LocalRef<@Nullable MomentInstance> ref) {
+        MomentInstance momentInstance = ref.get();
+
+        if (momentInstance != null) originalSize = momentInstance.getMoment().clientSettings()
                 .flatMap(ClientSettings::clientMoonSettings)
                 .flatMap(ClientMoonSettings::moonSize)
                 .orElse(originalSize);
@@ -41,8 +50,8 @@ public abstract class LevelRendererMixin {
     }
 
     @WrapOperation(method = "renderSky", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/resources/ResourceLocation;)V", ordinal = 1))
-    private void renderSky(int shaderTexture, ResourceLocation textureId, Operation<Void> original) {
-        MomentInstance instance = MomentInstanceManager.of(level).getClientMomentInstance();
+    private void renderSky(int shaderTexture, ResourceLocation textureId, Operation<Void> original, @Share("momentInstance") LocalRef<@Nullable MomentInstance> ref) {
+        MomentInstance instance = ref.get();
 
         if (instance != null) textureId = instance.getMoment().clientSettings()
                 .flatMap(ClientSettings::clientMoonSettings)
@@ -53,8 +62,8 @@ public abstract class LevelRendererMixin {
     }
 
     @Inject(method = "renderSky", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getMoonPhase()I"))
-    private void renderSky(Matrix4f frustumMatrix, Matrix4f projectionMatrix, float partialTick, Camera camera, boolean isFoggy, Runnable skyFogSetup, CallbackInfo ci) {
-        MomentInstance instance = MomentInstanceManager.of(level).getClientMomentInstance();
+    private void renderSky(Matrix4f frustumMatrix, Matrix4f projectionMatrix, float partialTick, Camera camera, boolean isFoggy, Runnable skyFogSetup, CallbackInfo ci, @Share("momentInstance") LocalRef<@Nullable MomentInstance> ref) {
+        MomentInstance instance = ref.get();
 
         if (instance != null) instance.getMoment().clientSettings()
                 .flatMap(ClientSettings::clientMoonSettings)
