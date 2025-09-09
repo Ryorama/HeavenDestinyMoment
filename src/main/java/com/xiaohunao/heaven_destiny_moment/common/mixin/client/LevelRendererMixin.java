a4a0c72c724f79ce9a53ba4135223c02f115a609
westernat <westernat233@163.com>
2025-09-09 09:39:42 +0800

    去除Redirect

--- src/main/java/com/xiaohunao/heaven_destiny_moment/common/mixin/client/LevelRendererMixin.java
+++ src/main/java/com/xiaohunao/heaven_destiny_moment/common/mixin/client/LevelRendererMixin.java
@@ -1,10 +1,11 @@
 package com.xiaohunao.heaven_destiny_moment.common.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.xiaohunao.heaven_destiny_moment.common.context.ClientMoonSettings;
 import com.xiaohunao.heaven_destiny_moment.common.context.ClientSettings;
 import com.xiaohunao.heaven_destiny_moment.common.moment.IMoment;
-import com.xiaohunao.heaven_destiny_moment.common.moment.Moment;
 import com.xiaohunao.heaven_destiny_moment.common.moment.MomentInstance;
 import com.xiaohunao.heaven_destiny_moment.common.moment.MomentInstanceManager;
 import com.xiaohunao.xhn_lib.common.util.ColorUtils;
@@ -16,14 +17,19 @@ import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
 
 @Mixin(LevelRenderer.class)
 public abstract class LevelRendererMixin {
-    @Shadow @Nullable private ClientLevel level;
+    @Shadow
+    @Nullable
+    private ClientLevel level;
 
     @ModifyConstant(method = "renderSky", constant = @Constant(floatValue = 20.0F))
     private float renderSky(float originalSize) {
@@ -41,18 +47,19 @@ public abstract class LevelRendererMixin {
         }
         return originalSize;
     }
-    @Redirect(method = "renderSky", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/resources/ResourceLocation;)V", ordinal = 1))
-    private void renderSky(int moonTextureId, ResourceLocation originaResourceLocation) {
+
+    @WrapOperation(method = "renderSky", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/resources/ResourceLocation;)V", ordinal = 1))
+    private void renderSky(int shaderTexture, ResourceLocation textureId, Operation<Void> original) {
         MomentInstanceManager momentInstanceManager = MomentInstanceManager.of(level);
 
-        ResourceLocation moonTexture = momentInstanceManager.getClientMomentInstance()
-                .map(MomentInstance::getMoment)
-                .flatMap(IMoment::clientSettings)
-                .flatMap(ClientSettings::clientMoonSettings)
-                .flatMap(ClientMoonSettings::moonTexture)
-                .orElse(originaResourceLocation);
+        if (momentInstanceManager.clientOnlyMomentInstance != null) {
+            textureId = momentInstanceManager.clientOnlyMomentInstance.getMoment().clientSettings()
+                    .flatMap(ClientSettings::clientMoonSettings)
+                    .flatMap(ClientMoonSettings::moonTexture)
+                    .orElse(textureId);
+        }
 
-        RenderSystem.setShaderTexture(moonTextureId, moonTexture);
+        original.call(shaderTexture, textureId);
     }
 
     @Inject(method = "renderSky", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getMoonPhase()I"))
@@ -67,9 +74,9 @@ public abstract class LevelRendererMixin {
                 .orElse(null);
 
 
-        if (moonColor != null){
+        if (moonColor != null) {
             Vector3f color = ColorUtils.colorToVector3f(moonColor);
-            RenderSystem.setShaderColor(color.x,color.y,color.z, 1.0F - level.getRainLevel(partialTick));
+            RenderSystem.setShaderColor(color.x, color.y, color.z, 1.0F - level.getRainLevel(partialTick));
         }
     }
 }
